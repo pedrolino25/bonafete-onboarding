dd910da4b81d71e5f3f6a7dec98f7b9b8b15bb28
pedrolino25 <pedrolino25@gmail.com>
2024-10-13 15:33:29 +0100

    revert

--- src/app/applications/new/page.tsx
+++ src/app/applications/new/page.tsx
@@ -1,17 +1,11 @@
 'use client'
 
+import ApplicationsListSection from '@/components/sections/applications-list/ApplicationsListSection'
 import {
   ApplicationStatus,
   getApplicationsListByStatus,
 } from '@/services/api/applications'
 import { useQuery } from '@tanstack/react-query'
-import dynamic from 'next/dynamic'
-
-const ApplicationsListSection = dynamic(
-  () =>
-    import('@/components/sections/applications-list/ApplicationsListSection'),
-  { ssr: false }
-)
 
 export default function Applications() {
   const { isPending, data, refetch } = useQuery({
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -30,71 +30,69 @@ export interface ImageType {
 
 export async function convertImageToWebP(file: File): Promise<ImageType> {
   return new Promise((resolve, reject) => {
-    if (document) {
-      const reader = new FileReader()
-      reader.readAsDataURL(file)
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
 
-      reader.onload = () => {
-        const img = new Image()
-        img.src = reader.result as string
+    reader.onload = () => {
+      const img = new Image()
+      img.src = reader.result as string
 
-        img.onload = () => {
-          const canvas = document.createElement('canvas')
-          const ctx = canvas.getContext('2d')
+      img.onload = () => {
+        const canvas = document.createElement('canvas')
+        const ctx = canvas.getContext('2d')
 
-          if (!ctx) {
-            reject(new Error('Could not create canvas context'))
-            return
-          }
-
-          // Max dimensions
-          const maxWidth = 1920
-          const maxHeight = 1080
-          let { width, height } = img
-
-          // Maintain aspect ratio
-          if (width > maxWidth || height > maxHeight) {
-            const aspectRatio = width / height
-
-            if (width > height) {
-              width = maxWidth
-              height = Math.round(maxWidth / aspectRatio)
-            } else {
-              height = maxHeight
-              width = Math.round(maxHeight * aspectRatio)
-            }
-          }
-
-          // Set the canvas size to the new dimensions
-          canvas.width = width
-          canvas.height = height
-
-          // Draw the image onto the canvas
-          ctx.drawImage(img, 0, 0, width, height)
-
-          // Convert the canvas to a WebP blob
-          canvas.toBlob(
-            (blob) => {
-              if (!blob) {
-                reject(new Error('Conversion to WebP failed'))
-                return
-              }
-
-              const url = URL.createObjectURL(blob)
-              resolve({
-                file: new File([blob], file.name, { type: 'image/webp' }),
-                path: url,
-              })
-            },
-            'image/webp',
-            0.75 // Quality (1.0 = max quality)
-          )
+        if (!ctx) {
+          reject(new Error('Could not create canvas context'))
+          return
         }
-      }
 
-      reader.onerror = () => {
-        reject(new Error('Error reading file'))
+        // Max dimensions
+        const maxWidth = 1920
+        const maxHeight = 1080
+        let { width, height } = img
+
+        // Maintain aspect ratio
+        if (width > maxWidth || height > maxHeight) {
+          const aspectRatio = width / height
+
+          if (width > height) {
+            width = maxWidth
+            height = Math.round(maxWidth / aspectRatio)
+          } else {
+            height = maxHeight
+            width = Math.round(maxHeight * aspectRatio)
+          }
+        }
+
+        // Set the canvas size to the new dimensions
+        canvas.width = width
+        canvas.height = height
+
+        // Draw the image onto the canvas
+        ctx.drawImage(img, 0, 0, width, height)
+
+        // Convert the canvas to a WebP blob
+        canvas.toBlob(
+          (blob) => {
+            if (!blob) {
+              reject(new Error('Conversion to WebP failed'))
+              return
+            }
+
+            const url = URL.createObjectURL(blob)
+            resolve({
+              file: new File([blob], file.name, { type: 'image/webp' }),
+              path: url,
+            })
+          },
+          'image/webp',
+          0.75 // Quality (1.0 = max quality)
+        )
       }
+    }
+
+    reader.onerror = () => {
+      reject(new Error('Error reading file'))
     }
   })
 }
